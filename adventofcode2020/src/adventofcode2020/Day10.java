aae23fac9a3b647e76072e0d253ad6af292c1ad2
Michael Werner <michael@4werners.net>
2021-01-03 14:08:41 -0500

    Day10 - cleanup and explanations

--- adventofcode2020/src/adventofcode2020/Day10.java
+++ adventofcode2020/src/adventofcode2020/Day10.java
@@ -8,24 +8,48 @@ import java.util.Collections;
 import java.util.List;
 
 public class Day10 {
+	//hold the combinations
+	public static List<String> sCombinations = new ArrayList<String>();
 	
 	public static long countPartial(List<Integer> iNumbers, int iStart, int iEnd) {
-		long iCombinations = 0;
-		System.out.println("Start: " + iStart + " End: " + iEnd);
-		if(iEnd - iStart == 1) {
-			iCombinations = 1;
+		long iCombinations = 1;
+		List<Integer> iNewList = new ArrayList<Integer>();
+		
+		/*
+        //test output of the current list
+		for(int i = iStart; i <= iEnd; i++) {
+			System.out.print(iNumbers.get(i));
+			if(i < iEnd) System.out.print(",");
 		}
-		else {
-			for(int i = iStart; i <= iEnd; i++) {
-				int j = i + 1;
-				if(j <= iEnd) {
-					if(iNumbers.get(j) - iNumbers.get(i) <= 2) {
-						iCombinations++;
-						iCombinations += countPartial(iNumbers, j, iEnd - 1);
-					}//if iNumbers
-				}//if j
-			}// for i
-		}// else
+		System.out.println();
+		*/
+		
+		for(int i = iStart; i <= iEnd; i += 1) {
+			// test the next but one number. The next number does not need 
+			// to be checked. It is always part of the solution
+			if(i + 2 < iEnd && iNumbers.get(i + 2) - iNumbers.get(i) <= 3) {
+				iNewList.clear();
+				//System.out.println("i is: " + i + " : " + iNumbers.toString());
+				
+				//create the new list, skip the number one before the number
+				//that was tested
+				for(int k = 0; k <= iEnd; k++) {
+					if(k != i + 1) {
+						iNewList.add(iNumbers.get(k));
+					}	
+				}// for k
+				
+				/* check it the current list has already been identified as
+				   a combination. If it has not, add it.
+				   It can happen, that a combination is created more than once.
+				*/
+				
+				if(!sCombinations.contains(iNewList.toString())) {
+					sCombinations.add(iNewList.toString());
+					iCombinations += countPartial(iNewList, iStart, iNewList.size() - 1);
+				}// if !sCombinations
+			}// if i + 2	
+		} // for i
 		
 		return iCombinations;
 	}// countPartial
@@ -39,18 +63,20 @@ public class Day10 {
 			int j = i + 1;
 			if(j < iNumbers.size()) {
 				if(iNumbers.get(j) - iNumbers.get(i) == 3) {
-					//i is now the end of the partial list
+					//j is the end of the partial list and the beginning of the next
 					//get the combinations for this part of the list 
-					
+
 					if(iStart == i) { //when there are consecutive jumps of 3, like 38, 41, 44
 						iCombinations = 1;
 					}
 					else {
-						iCombinations = countPartial(iNumbers, iStart, i);
-					}
+						//no need to keep the combinations of the previous run
+						sCombinations.clear();
+						iCombinations = countPartial(iNumbers, iStart, j);
+					}// if iSart
 					
 					iTotalCombinations = iTotalCombinations * iCombinations;
-					System.out.println("Start: " + iStart + ", End: " + i + ", Combinations: " + iCombinations + ", Total: " + iTotalCombinations);
+					//System.out.println("Start: " + iStart + ", End: " + j + ", Combinations: " + iCombinations + ", Total: " + iTotalCombinations);
 					
 					//j is the start of the next part, save that value
 					iStart = j;
@@ -67,13 +93,9 @@ public class Day10 {
 		int iThree = 0; 
 
     	for(int i = 0; i < iNumbers.size() - 1; i++) {
-    		if(iNumbers.get(i + 1) - iNumbers.get(i) == 1) {
-    			iOne++;
-    		}
+    		if(iNumbers.get(i + 1) - iNumbers.get(i) == 1) 	iOne++;
     		
-    		if(iNumbers.get(i + 1) - iNumbers.get(i) == 3) {
-    			iThree++;
-    		}
+    		if(iNumbers.get(i + 1) - iNumbers.get(i) == 3) 	iThree++;
     		
     	}// for i
     	
@@ -87,7 +109,7 @@ public class Day10 {
 		//add the outlet
 		iNumbers.add(0);
 		
-    	try (BufferedReader br = new BufferedReader(new FileReader(".\\input\\day10a.txt"))){
+    	try (BufferedReader br = new BufferedReader(new FileReader(".\\input\\day10.txt"))){
             while ((input = br.readLine()) != null) {
                 if (input.trim().length() > 0) {
                 	iNumbers.add(Integer.parseInt(input));
@@ -96,7 +118,20 @@ public class Day10 {
 		} catch (IOException e) {
 			 System.out.println(e.getMessage());
 	      } //catch
-
+ 
+    	/*
+    	// a small test case. The result is 14. (7 * 2)
+    	iNumbers.clear();
+    	iNumbers.add(0);
+    	iNumbers.add(1);
+    	iNumbers.add(2);
+    	iNumbers.add(3);
+    	iNumbers.add(4);
+    	iNumbers.add(7);
+    	iNumbers.add(9);
+    	iNumbers.add(10);
+       	*/
+    	
     	//add the end point
     	Collections.sort(iNumbers);
     	iNumbers.add(iNumbers.get(iNumbers.size() - 1) + 3);
