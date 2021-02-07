dd9e415ef3c8e9be1a877a9575bd9f6cfa5ca82e
Michael Werner <michael@4werners.net>
2021-02-07 16:09:41 -0500

    Adding a scene
    to show something on the stage

--- jenkov.tutorial/src/main/java/module-info.java
+++ jenkov.tutorial/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module net._4werners.jenkov.tutorial {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens net._4werners.jenkov.tutorial to javafx.fxml;
     exports net._4werners.jenkov.tutorial;
--- jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
+++ jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
@@ -3,6 +3,8 @@
 package net._4werners.jenkov.tutorial;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MyFxApp extends Application {
@@ -10,6 +12,10 @@ public class MyFxApp extends Application {
     @Override
         public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("My First JavaFX App");
+        
+        Label label = new Label("Hello World, JavaFX !");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
 
         primaryStage.show();
     }
