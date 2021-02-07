3408726839f6b17c60677991107c3d4ca277d7d1
Michael Werner <michael@4werners.net>
2021-02-07 16:35:05 -0500

    modal stages

--- jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
+++ jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
@@ -5,6 +5,7 @@ package net._4werners.jenkov.tutorial;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MyFxApp extends Application {
@@ -12,15 +13,26 @@ public class MyFxApp extends Application {
     @Override
         public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("My First JavaFX App");
-        primaryStage.setX(500);
-        primaryStage.setY(150);
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(100);
+
         Label label = new Label("Hello World, JavaFX !");
         Scene scene = new Scene(label, 400, 200);
         primaryStage.setScene(scene);
 
+        Stage stage = new Stage();
+        
+        stage.setWidth(300);
+        stage.setHeight(100);
+        stage.initModality(Modality.APPLICATION_MODAL);  //blocks all other stages
+        //stage.initModality(Modality.WINDOW_MODAL);     //blocks the owner of this stage
+        //stage.initModality(Modality.NONE);             //doesn't block anything
+        
+        Label secondLabel = new Label("close me to continue !");
+        Scene secondScene = new Scene(secondLabel, 400, 200);
+        stage.setScene(secondScene);
+        
         primaryStage.show();
+        
+        stage.showAndWait();  //waits until the stage is closed. Useful to execute code after that.
     }
     
     public static void main(String[] args) {
