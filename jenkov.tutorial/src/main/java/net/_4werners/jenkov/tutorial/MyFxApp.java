78101a1494beefc64a719dd4f643dee29a975d3c
Michael Werner <michael@4werners.net>
2021-02-07 16:49:56 -0500

    Owner and modality

--- jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
+++ jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
@@ -17,22 +17,40 @@ public class MyFxApp extends Application {
         Label label = new Label("Hello World, JavaFX !");
         Scene scene = new Scene(label, 400, 200);
         primaryStage.setScene(scene);
+        primaryStage.setX(50);
+        primaryStage.setY(100);
 
-        Stage stage = new Stage();
+        Stage secondStage = new Stage();
         
-        stage.setWidth(300);
-        stage.setHeight(100);
-        stage.initModality(Modality.APPLICATION_MODAL);  //blocks all other stages
+        secondStage.setWidth(300);
+        secondStage.setHeight(100);
+        secondStage.setX(100);
+        secondStage.setY(200);
+        secondStage.setTitle("second stage");
+        //secondStage.initModality(Modality.APPLICATION_MODAL);  //blocks all other stages
         //stage.initModality(Modality.WINDOW_MODAL);     //blocks the owner of this stage
         //stage.initModality(Modality.NONE);             //doesn't block anything
         
         Label secondLabel = new Label("close me to continue !");
         Scene secondScene = new Scene(secondLabel, 400, 200);
-        stage.setScene(secondScene);
+        secondStage.setScene(secondScene);
+
+        Stage thirdStage = new Stage();
+        thirdStage.setWidth(300);
+        thirdStage.setHeight(150);
+        thirdStage.setX(200);
+        thirdStage.setY(400);
+        thirdStage.setTitle("third");
+        thirdStage.initOwner(secondStage);
+        thirdStage.initModality(Modality.WINDOW_MODAL);
+        Label thirdLabel = new Label("you can click on the first stage\nbut not on the second");
+        Scene thirdScene = new Scene(thirdLabel, 400, 200);
+        thirdStage.setScene(thirdScene);
+        		
         
         primaryStage.show();
-        
-        stage.showAndWait();  //waits until the stage is closed. Useful to execute code after that.
+        secondStage.show();  //waits until the stage is closed. Useful to execute code after that.
+        thirdStage.show();
     }
     
     public static void main(String[] args) {
