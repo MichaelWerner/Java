f3bb3088bf281d3ae24a9908d9be5f99c3ac6bfc
Michael Werner <michael@4werners.net>
2021-02-07 17:03:57 -0500

    event listeners

--- jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
+++ jenkov.tutorial/src/main/java/net/_4werners/jenkov/tutorial/MyFxApp.java
@@ -5,6 +5,7 @@ package net._4werners.jenkov.tutorial;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -47,6 +48,47 @@ public class MyFxApp extends Application {
         Scene thirdScene = new Scene(thirdLabel, 400, 200);
         thirdStage.setScene(thirdScene);
         		
+        thirdStage.setOnCloseRequest((event) -> {
+            System.out.println("Closing Stage 3");
+        });
+        
+        //hiding = closing
+        secondStage.setOnHiding((event) -> {
+            System.out.println("Hiding Stage 2");
+        });
+        
+        secondStage.setOnHidden((event) -> {
+            System.out.println("Stage 2 hidden");
+        });
+        
+        primaryStage.setOnShowing((event) -> {
+            System.out.println("Showing Stage  1");
+        });
+        
+        primaryStage.setOnShown((event) -> {
+            System.out.println("Stage 1 Shown");
+        });
+        
+        
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED,  (event) -> {
+            System.out.println("Key pressed: " + event.toString());
+
+            switch(event.getCode().getCode()) {
+                case 27 : { // 27 = ESC key
+                	System.out.println("Esc pressed");
+                    primaryStage.close();
+                    break;
+                }
+                case 10 : { // 10 = Return
+                	System.out.println("Return pressed");
+                    primaryStage.setWidth( primaryStage.getWidth() * 2);
+                }
+                default:  {
+                    System.out.println("Unrecognized key");
+                }
+            }
+        });        
+        
         
         primaryStage.show();
         secondStage.show();  //waits until the stage is closed. Useful to execute code after that.
