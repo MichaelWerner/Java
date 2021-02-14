a289862196d608c65ab97a8652510cf600ad2d48
Michael Werner <michael@4werners.net>
2021-02-14 10:17:22 -0500

    Properties

--- jenkov.tutorial/src/main/java/module-info.java
+++ jenkov.tutorial/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module net._4werners.jenkov.tutorial {
     requires javafx.controls;
     requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 
     opens net._4werners.jenkov.tutorial to javafx.fxml;
     exports net._4werners.jenkov.tutorial;
